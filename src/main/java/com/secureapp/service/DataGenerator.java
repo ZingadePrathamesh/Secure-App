93174e353844e530b3581fad887b2e237dd6493a
Prathamesh Zingade <zingadeprathamesh12@gmail.com>
2025-05-26 08:40:41 +0530

    Generate fake data

---
+++ src/main/java/com/secureapp/service/DataGenerator.java
@@ -0,0 +1,60 @@
+package com.secureapp.service;
+
+import com.secureapp.enums.Role;
+import com.secureapp.model.UserProfile;
+import com.secureapp.respository.UserRepository;
+import net.datafaker.Faker;
+import org.apache.commons.text.RandomStringGenerator;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.w3c.dom.CharacterData;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class DataGenerator {
+
+    @Bean
+    @Order(value = 1)
+    public CommandLineRunner insertUserProfiles(
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder
+    ){
+        return args -> {
+            if(userRepository.count() > 0){
+                return;
+            }
+            Faker faker = new Faker();
+            Set<UserProfile> userProfiles = new HashSet<>();
+            for(int i = 0; i < 100; i++){
+                String firstname = faker.name().firstName();
+                String lastname = faker.name().lastName();
+                String password = generateRandomSpecialCharacters(10);
+
+                userProfiles.add(UserProfile.builder()
+                                .createdAt(LocalDateTime.now())
+                                .accountLocked(false)
+                                .enabled(true)
+                                .email(firstname + "."+ lastname + i + "@gmail.com")
+                                .password(passwordEncoder.encode(password))
+                                .firstName(firstname)
+                                .lastName(lastname)
+                                .id(null)
+                                .role(Role.STUDENT)
+                        .build());
+            }
+            userRepository.saveAll(userProfiles);
+        };
+    }
+
+    public String generateRandomSpecialCharacters(int length) {
+        RandomStringGenerator pwdGenerator = new RandomStringGenerator.Builder().withinRange(33, 45)
+                .build();
+        return pwdGenerator.generate(length);
+    }
+}
